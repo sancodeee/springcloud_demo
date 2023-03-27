38f412625242f216f18ec26908d3793958b63d11
wangsen <wangsen_a@aspirecn.com>
2023-03-27 18:10:58 +0800

    更新

--- springcloud_provider_add/src/main/java/com/ws/controller/AddBookController.java
+++ springcloud_provider_add/src/main/java/com/ws/controller/AddBookController.java
@@ -5,9 +5,14 @@ import com.ws.entity.Book;
 import com.ws.service.AddAuthorService;
 import com.ws.service.AddBookService;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/book")
 public class AddBookController {
@@ -21,23 +26,35 @@ public class AddBookController {
     @ApiOperation(value = "添加书籍信息")
     @PostMapping("/addBook")
     public String addBook(@RequestBody(required = true) Book book) {
-        boolean b = addBookService.addBook(book);
-        if (b) {
-            return "添加成功！";
-        } else {
-            return "添加失败!";
+        try {
+            boolean b = addBookService.addBook(book);
+            if (b) {
+                return "添加成功！";
+            } else {
+                return "添加失败!";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "添加失败，该id已存在数据库！";
         }
     }
 
     @ApiOperation(value = "添加书籍作者信息")
     @PostMapping("/addAuthor")
-    public String addAuthor(@RequestBody(required = true)Author author){
-        boolean b = addAuthorService.addAuthor(author);
-        if (b){
-            return "添加成功";
-        }else {
-            return "添加失败";
+    public String addAuthor(@RequestBody(required = true) Author author) {
+        try {
+            boolean b = addAuthorService.addAuthor(author);
+            if (b) {
+                return "添加成功";
+            } else {
+                return "添加失败";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("该id已存在！");
+            return "添加失败，该id已存在数据库";
         }
     }
+
 
 }
